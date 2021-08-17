aaeceee3f6829591fa373eed01e4b294bec6c921
Ayush193 <74961334+Ayush193@users.noreply.github.com>
2021-08-17 15:13:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var backgroundImage, canvas;
+var database
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+
+var game,player,form
+var car1,car2,car3,car4,cars
+var track, car1_img,car2_img,car3_img,car4_img;
+function preload (){
+car1_img = loadImage ("/img")
+
+
+
+}
+
+
+
+
+
+
+
+
